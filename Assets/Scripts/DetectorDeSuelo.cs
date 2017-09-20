b845e8557c4f87044c01acd7f4417f4f38a78851
Luigui Mendoza <ludk17@gmail.com>
2017-09-20 07:42:17 -0500

    first commit

---
+++ Assets/Scripts/DetectorDeSuelo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorDeSuelo : MonoBehaviour {
+
+	public Jugador jugador;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		
+		jugador.saltos = 0;
+		jugador.estaEnElSuelo = true;
+		jugador.animator.SetInteger ("Estado", 0);
+		Debug.Log ("colision");
+
+	}
+}
---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+
+	public int velocidad;
+	public int fuerzaSalto;
+
+	private Rigidbody2D rb;
+	public Animator animator;
+
+	private const int QUIETO = 0;
+	private const int CORRER = 1;
+	private const int SALTAR = 2;
+
+	public int saltos = 0;
+	public bool estaEnElSuelo = true;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow) && estaEnElSuelo) {
+			animator.SetInteger ("Estado", CORRER);
+			rb.velocity = new Vector2(velocidad, rb.velocity.y);
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			animator.SetInteger ("Estado", QUIETO);
+			rb.velocity = new Vector2(0, rb.velocity.y);
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			animator.SetInteger ("Estado", CORRER);
+			rb.velocity = new Vector2(velocidad * -1, rb.velocity.y);
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			animator.SetInteger ("Estado", QUIETO);
+			rb.velocity = new Vector2(0, rb.velocity.y);
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && saltos < 2) {
+			animator.SetInteger ("Estado", SALTAR);
+			rb.velocity = new Vector2(rb.velocity.x ,fuerzaSalto);
+			estaEnElSuelo = false;
+			saltos++;
+		}
+
+		//else {			
+		//	animator.SetInteger ("Estado", QUIETO);
+		//}
+	}
+
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if(col.gameObject.tag == "Enemigo")
+		{
+			Debug.Log ("Morir");
+		}
+		//else {
+			//saltos = 0;
+			//estaEnElSuelo = true;
+			//animator.SetInteger ("Estado", QUIETO);
+			//Debug.Log ("colision");
+		//}
+	}
+}
