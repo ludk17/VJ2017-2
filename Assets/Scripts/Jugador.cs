bbeef415bff00fcb44808139f41de78e391b9276
Luigui Mendoza <ludk17@gmail.com>
2017-09-26 09:01:34 -0500

    add new animations

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -14,10 +14,13 @@ public class Jugador : MonoBehaviour {
 	private const int CORRER = 1;
 	private const int SALTAR = 2;
 	private const int ATACAR = 3;
+	private const int PLANEAR = 4;
 
 	public int saltos = 0;
 	public bool estaEnElSuelo = true;
 	public bool estaAtacando = false;
+
+	private int estado = 0;
 
 
 	// Use this for initialization
@@ -28,30 +31,11 @@ public class Jugador : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.RightArrow) && estaEnElSuelo) {
-			animator.SetInteger ("Estado", CORRER);
-			rb.velocity = new Vector2(velocidad, rb.velocity.y);
-			transform.localScale = new Vector3(1, 1, 1);
-		}
 
-		if (Input.GetKeyUp (KeyCode.RightArrow)) {
-			animator.SetInteger ("Estado", QUIETO);
-			rb.velocity = new Vector2(0, rb.velocity.y);
-			transform.localScale = new Vector3(1, 1, 1);
-		}
+		MoverseALaDerecha ();
+		MoverALaIzquierda();
 
 
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			animator.SetInteger ("Estado", CORRER);
-			rb.velocity = new Vector2(velocidad * -1, rb.velocity.y);
-			transform.localScale = new Vector3(-1, 1, 1);
-		}
-
-		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
-			animator.SetInteger ("Estado", QUIETO);
-			rb.velocity = new Vector2(0, rb.velocity.y);
-			transform.localScale = new Vector3(-1, 1, 1);
-		}
 
 		if (Input.GetKeyDown (KeyCode.Space) && saltos < 2) {
 			animator.SetInteger ("Estado", SALTAR);
@@ -60,20 +44,35 @@ public class Jugador : MonoBehaviour {
 			saltos++;
 		}
 
-		if (Input.GetKey (KeyCode.X)) {
-			animator.SetInteger ("Estado", ATACAR);		
+		if (Input.GetKeyDown (KeyCode.X)) {			
+			Debug.Log ("Atacar");
+			animator.SetInteger ("Estado", ATACAR);
 			estaAtacando = true;
 		}
 
-		if (Input.GetKeyUp (KeyCode.X)) {
-			animator.SetInteger ("Estado", QUIETO);		
+		if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.5 && 
+			!animator.IsInTransition(0) && 
+			animator.GetCurrentAnimatorStateInfo(0).IsName("Atack")) {
 			estaAtacando = false;
+			animator.SetInteger ("Estado", QUIETO);
 		}
 
 
-		//else {			
-		//	animator.SetInteger ("Estado", QUIETO);
+		if (Input.GetKey (KeyCode.Z)) {			
+			rb.gravityScale = (float)0.2;
+			animator.SetInteger ("Estado", PLANEAR);
+		}
+
+		if (Input.GetKeyUp (KeyCode.Z)) {
+			rb.gravityScale = (float)1;
+			animator.SetInteger ("Estado", QUIETO);
+		}
+
+		//if (Input.GetKeyUp (KeyCode.X)) {
+			//animator.SetInteger ("Estado", QUIETO);		
+			//estaAtacando = false;
 		//}
+
 	}
 
 
@@ -88,11 +87,39 @@ public class Jugador : MonoBehaviour {
 				Destroy (collision.gameObject);
 			}
 		}
-		//else {
-			//saltos = 0;
-			//estaEnElSuelo = true;
-			//animator.SetInteger ("Estado", QUIETO);
-			//Debug.Log ("colision");
-		//}
+
+	}
+
+	private void MoverseALaDerecha()
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			animator.SetInteger ("Estado", CORRER);
+			rb.velocity = new Vector2(velocidad, rb.velocity.y);
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			animator.SetInteger ("Estado", QUIETO);
+			rb.velocity = new Vector2(0, rb.velocity.y);
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+
+	}
+
+	private void MoverALaIzquierda()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			animator.SetInteger ("Estado", CORRER);
+			rb.velocity = new Vector2(velocidad * -1, rb.velocity.y);
+			transform.localScale = new Vector3(-1, 1, 1);
+			estado = CORRER;
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			animator.SetInteger ("Estado", QUIETO);
+			rb.velocity = new Vector2(0, rb.velocity.y);
+			transform.localScale = new Vector3(-1, 1, 1);
+			estado = QUIETO;
+		}
 	}
 }
